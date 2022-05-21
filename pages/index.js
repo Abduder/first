2798b1ca551466e1e3f598247c35ea931fe43ca0
Abduder <97856502+Abduder@users.noreply.github.com>
2022-05-21 18:54:50 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -77,7 +77,7 @@ export default function Home({ isConnected }) {
 
 export async function getServerSideProps(context) {
   try {
-    await connectToDatabase
+    await connectToDatabase();
     // `await clientPromise` will use the default database passed in the MONGODB_URI
     // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
     //
